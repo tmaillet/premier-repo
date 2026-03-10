598c156dd94b8083a3aa3d9ea1e48b6e1e7ac7c3
tmaillet <theo.mailletgronnier@gmail.com>
2026-03-10 14:11:44 +0100

    TD5

---
+++ MesDates.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Classe MesDates pour l'exercice du TD5.
+ */
+class MesDates
+{
+    /**
+     * Retourne la date de demain au format JSON.
+     *
+     * @return string
+     */
+    public function demain(): string
+    {
+        $date = new DateTime('tomorrow');
+        return json_encode([
+            'demain' => $date->format('d-m-Y')
+        ]);
+    }
+}
